018a3bebbe280faccd8bdeb0ad15cdc2e07e79fc
Levickiy-hub <55253795+Levickiy-hub@users.noreply.github.com>
2020-12-17 10:51:52 +0300

    Add files via upload

---
+++ squ.sql
@@ -0,0 +1,83 @@
+create sequence CAR_S
+    increment by 1
+    start with 0
+    nomaxvalue
+    minvalue 0
+    nocycle
+    nocache;
+
+create sequence USER_S
+    increment by 1
+    start with 1
+    nomaxvalue
+    minvalue 0
+    nocycle
+    nocache;
+    
+    create sequence BRAND_S
+    increment by 1
+    start with 0
+    nomaxvalue
+    minvalue 0
+    nocycle
+    nocache;
+    
+    create sequence MODEL_S
+    increment by 1
+    start with 0
+    nomaxvalue
+    minvalue 0
+    nocycle
+    nocache;
+    
+    
+    create sequence COMMENT_S
+    increment by 1
+    start with 0
+    nomaxvalue
+    minvalue 0
+    nocycle
+    nocache;
+    
+    
+    create sequence CONTACT_S
+    increment by 1
+    start with 0
+    nomaxvalue
+    minvalue 0
+    nocycle
+    nocache;
+    
+    
+    
+    create sequence PICTURE_S
+    increment by 1
+    start with 0
+    nomaxvalue
+    minvalue 0
+    nocycle
+    nocache;
+    
+    create sequence POST_S
+    increment by 1
+    start with 0
+    nomaxvalue
+    minvalue 0
+    nocycle
+    nocache;
+    
+     create sequence SAVED_S
+    increment by 1
+    start with 0
+    nomaxvalue
+    minvalue 0
+    nocycle
+    nocache;
+    
+     create sequence SPARE_S
+    increment by 1
+    start with 0
+    nomaxvalue
+    minvalue 0
+    nocycle
+    nocache;
